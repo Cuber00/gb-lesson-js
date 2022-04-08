bc81861f03b85f108cfa96c4ac2527be5ddeda6d
Cuber00 <snidak2018@yandex.ru>
2022-04-08 10:07:59 +0700

    Solving the five task

--- script.js
+++ script.js
@@ -30,19 +30,19 @@
 // console.log(arrNew);
 
 // Task #4
-// 4
-// Создайте массив, состоящий из 5 элементов. (Элементами массива должны быть произвольные цифры в диапазоне от 0 до 10.)
-// Удалите значение 3, если будет такое значение.
-// Каждое значение исходного элемента массива необходимо увеличить на 10%.
-// Выведите в консоль два этих массива.
 
+// const arr = new Array();    
+// for (let i=0; i < 5; i++) {
+//     const random = () => Math.floor(Math.random()*10);
+//     arr.push(random());
+// }
+// const arrNew = arr.filter(item => item !== 3).map(item => +(item * 1.1).toFixed(2))
+// console.log(arr);
+// console.log(arrNew);
 
-const arr = new Array();    
-for (let i=0; i < 5; i++) {
-    const random = () => Math.floor(Math.random()*10);
-    arr.push(random());
+//Task #5
+
+for(let i=0; i < 10; console.log(i), i++){
+
 }
 
-const arrNew = arr.filter(item => item !== 3).map(item => +(item * 1.1).toFixed(2))
-console.log(arr);
-console.log(arrNew);
